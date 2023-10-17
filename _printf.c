20759fe178b493ea94088ccedb6129b5c0aa0082
devmike93 <msk.megbenu@gmail.com>
2023-10-17 19:24:11 +0200

    add _printf

---
+++ _printf.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdarg.h>
+
+/**
+ * _printf - print any type of variable
+ * @format: is a character string
+ * Return: the number of characters printed
+ **/
+
+int _printf(const char *format, ...)
+{
+	int total = 0;
+	va_list list;
+
+
+	va_start(list, format);
+	while (*format != '\0')
+	{
+		if (*format == '%' && *(format + 1) != '\0')
+			total += get_conv(format, list);
+		else
+		{
+			total += print_chr(*format);
+		}
+		++format;
+	}
+	va_end(list);
+	return (total);
+}
